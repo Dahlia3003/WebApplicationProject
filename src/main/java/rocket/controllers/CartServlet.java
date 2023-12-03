b3e7015e6e1d87f1a73b1120cb18852390efc0d6
Dahlia3003 <duyvnlx3016@gmail.com>
2023-12-04 00:07:33 +0700

    Đêm khổ dâm đầu tiên

--- src/main/java/rocket/controllers/CartServlet.java
+++ src/main/java/rocket/controllers/CartServlet.java
@@ -1,48 +1,45 @@
 package rocket.controllers;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import rocket.Util.DBUtil;
+import rocket.data.CartDB;
 import rocket.models.Cart;
+import rocket.models.CartLine;
 
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "cartServlet", value = "/cart")
-public class CartServlet {
-    // Trong phương thức doGet của CartServlet
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.getEmf().createEntityManager();
-        try {
-            // Bắt đầu giao dịch
-            em.getTransaction().begin();
+@WebServlet(name = "cartServlet", value = "/views/cart")
+public class CartServlet extends HttpServlet {
 
-            // Truy vấn cơ sở dữ liệu để lấy danh sách Cart và CartLine
-            Query query = em.createQuery("SELECT c FROM Cart c WHERE c.id = :cartId", Cart.class);
-            query.setParameter("cartId", "1");
-            Cart cart = (Cart) query.getSingleResult();
-            // Kết thúc giao dịch
-            em.getTransaction().commit();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String cartID = "6";  // Có thể lấy từ request.getParameter("cartID");
 
-            // Đặt danh sách Cart và CartLine vào request attribute
+        List<CartLine> cartLines = CartDB.getCartLinesByCartID(cartID);
+
+
+        if (cartLines != null) {
+            Cart cart = new Cart();
+            cart.setCartList(cartLines);
+
             request.setAttribute("cart", cart);
-        } catch (Exception e) {
-            // Xử lý ngoại lệ nếu có bất kỳ lỗi nào xảy ra
-            em.getTransaction().rollback();
-            e.printStackTrace(); // Thay bằng xử lý lỗi phù hợp trong ứng dụng của bạn
-        } finally {
-            // Đóng EntityManager sau khi hoàn thành
-            em.close();
+            System.out.println("cartServlet");
+        } else {
+            System.out.println("Null");
         }
 
-        // Chuyển hướng đến trang Cart.jsp
         RequestDispatcher dispatcher = request.getRequestDispatcher("/views/Cart.jsp");
         dispatcher.forward(request, response);
     }
-}
 
+
+}
--- src/main/java/rocket/data/CartDB.java
+++ src/main/java/rocket/data/CartDB.java
@@ -2,6 +2,7 @@ package rocket.data;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
 import rocket.models.Cart;
 import rocket.models.CartLine;
 import rocket.Util.DBUtil;
@@ -10,6 +11,15 @@ import java.util.List;
 
 
 public class CartDB {
+
+    public static Cart getCartById(Integer cartID) {
+        EntityManager em = DBUtil.getEmf().createEntityManager();
+        try {
+            return em.find(Cart.class, cartID);
+        } finally {
+            em.close();
+        }
+    }
 
     public static void addCartItem(String cartID, CartLine cartline)
     {
@@ -33,56 +43,56 @@ public class CartDB {
         }
     }
 
-    public static void removeCartItem(String cartID, CartLine cartline)
-    {
+    public static void removeCartItem(Integer cartID, CartLine cartLineToRemove) {
         EntityManager em = DBUtil.getEmf().createEntityManager();
         EntityTransaction trans = em.getTransaction();
+
         try {
             trans.begin();
+
+            // Retrieve the Cart by its ID
             Cart cart = em.find(Cart.class, cartID);
-            if (cart != null) {
-                List<CartLine> cartList = cart.getCartList();
-                cartList.remove(cartline);
+
+            if (cart != null && cartLineToRemove != null) {
+                // Remove the specified CartLine from the Cart's cartList
+                cart.getCartList().remove(cartLineToRemove);
             }
+
             trans.commit();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             trans.rollback();
-        }
-        finally {
+            e.printStackTrace(); // Handle or log the exception appropriately
+        } finally {
             em.close();
         }
     }
 
-    public static void updateQuantity(String cartID, String cartlineID, Integer quantity)
-    {
+    public static void updateQuantity(Integer cartlineID, Integer quantity) {
         EntityManager em = DBUtil.getEmf().createEntityManager();
         EntityTransaction trans = em.getTransaction();
+
         try {
             trans.begin();
-            Cart cart = em.find(Cart.class, cartID);
-            if (cart != null)
-            {
-                List<CartLine> cartList = cart.getCartList();
-                    for(CartLine cartline : cartList)
-                    {
-                        if (cartline.getId().equals(cartlineID))
-                        {
-                            cartline.setQuantity(quantity);
-                        }
-                    }
+
+            // Find the CartLine directly by cartlineID
+            CartLine cartline = em.find(CartLine.class, cartlineID);
+
+            if (cartline != null) {
+                System.out.println("Updating quantity for CartLine ID: " + cartlineID + " to " + quantity);
+                cartline.setQuantity(quantity);
+                // Recalculate unitCost based on the updated quantity
+                cartline.setUniCost(cartline.calcPrice());
             }
+
             trans.commit();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             trans.rollback();
-        }
-        finally {
+            e.printStackTrace(); // Print the exception stack trace for debugging
+        } finally {
             em.close();
         }
     }
+
 
     public static void updateVariation(String cartID, String cartlineID, String variation)
     {
@@ -112,6 +122,22 @@ public class CartDB {
             em.close();
         }
     }
+    public static List<CartLine> getCartLinesByCartID(String cartID) {
+        EntityManager em = DBUtil.getEmf().createEntityManager();
+        try {
+            Cart cart = em.find(Cart.class, Integer.parseInt(cartID)); // Convert cartID to appropriate type
+            if (cart != null) {
+                List<CartLine> cartLines = cart.getCartList();
+
+                return cartLines;
+            } else {
+                return null; // or an empty list depending on your use case
+            }
+        } finally {
+            em.close();
+        }
+    }
+
 
 
 }
