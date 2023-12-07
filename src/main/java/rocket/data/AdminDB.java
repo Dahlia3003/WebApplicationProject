379e0e916e092fdf5af21d71c29f68107e5a7ad1
Dahlia3003 <duyvnlx3016@gmail.com>
2023-12-07 09:25:05 +0700

    admin

---
+++ src/main/java/rocket/data/AdminDB.java
@@ -0,0 +1,19 @@
+package rocket.data;
+
+
+import jakarta.persistence.*;
+import rocket.models.Account;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import rocket.models.Cart;
+
+import rocket.models.Customer;
+import rocket.Util.DBUtil;
+
+import java.util.List;
+
+public class AdminDB {
+
+}
